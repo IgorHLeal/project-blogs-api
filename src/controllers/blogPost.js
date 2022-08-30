2472ddfedde01a81ee08c3ee7e4a024949c04a78
Igor Leal <igorhenriqueleal3@gmail.com>
2022-08-29 21:09:40 -0300

    feat: Iniciada a criação do endpoint GET /post:id

--- src/controllers/blogPost.js
+++ src/controllers/blogPost.js
@@ -5,6 +5,14 @@ const blogPostController = {
       const blogPosts = await blogPostService.getAll();
       return res.status(200).json(blogPosts);
   },
+
+  getById: async (req, res) => {
+    const { id } = req.params;
+    const post = await blogPostService.getById(id);
+    if (!post) return res.status(404).json({ message: 'Post does not exist' });
+
+    return res.status(200).json(post);
+  },
 };
 
 module.exports = blogPostController;
--- src/routers/blogPostRouter.js
+++ src/routers/blogPostRouter.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const { getAll } = require('../controllers/blogPost');
 const tokenAuth = require('../middlewares/auth');
+const { getById } = require('../services/blogPost');
 
 const blogPostRouter = Router();
 
 blogPostRouter.get('/', tokenAuth, getAll);
+blogPostRouter.get('/:id', tokenAuth, getById);
 
 module.exports = blogPostRouter;
